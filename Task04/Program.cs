1859309c104453ab6767ebfc7270998944da1a7f
olga <olgamatvey@gmail.com>
2023-08-08 15:44:26 +0300

    Correct error04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -6,13 +6,14 @@
 
 
 Console.WriteLine("Введите первое число ");
-
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
+
 if (number1 > number2 && number1 > number3)
 {
     Console.WriteLine($"Максимальное число = {number1}");
