8169faae5945f6aefa1f89e01c97487f182f9b74
olga <olgamatvey@gmail.com>
2023-08-09 18:46:03 +0300

    Added Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,17 +4,24 @@
 // 782 -> 8
 // 918 -> 1
 
-int number = new Random().Next(100, 1000); 
-Console.WriteLine($"Случайное число из диапазона 100 - 999 -> {number}");
+
+Console.WriteLine("Введите трехзначноне число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 99 || number > 1000)
+{
+    Console.WriteLine($"Некорректное число. Повторите попытку "); 
+    number = Convert.ToInt32(Console.ReadLine());
+}
 
 int number1 = PrintSecondDigital(number);
 Console.WriteLine($"Второе число: {number1}");
 
 int PrintSecondDigital(int num)
-{
+ {
     int first = num % 100;
     int second = num % 10;
     int result = (first  - second) / 10;
-
-    return result;
+    return result;   
 }
+
