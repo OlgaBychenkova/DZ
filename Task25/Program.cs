f35c29f264c98028d4bb32af408789528bc4c012
olga <olgamatvey@gmail.com>
2023-08-28 21:37:10 +0300

    Added Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int PowNumber(int num, int n)
+{
+    int res = 1;
+    for(int i = 0; i < n; i++)
+    {
+        res *= num;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = PowNumber(number1, number2);
+Console.WriteLine($"{number1} в степени {number2} -> {result}");
